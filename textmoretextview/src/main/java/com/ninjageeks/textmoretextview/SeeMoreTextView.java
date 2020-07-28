3f072e7ce8086472ca98b5027e658f1f8db49e27
mahdi <mahdicsharp0098@gmail.com>
2020-07-28 13:28:50 +0430

    Finish See More With Lines Count

--- textmoretextview/src/main/java/com/ninjageeks/textmoretextview/SeeMoreTextView.java
+++ textmoretextview/src/main/java/com/ninjageeks/textmoretextview/SeeMoreTextView.java
@@ -9,9 +9,13 @@ import android.text.TextPaint;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
@@ -28,12 +32,16 @@ public class SeeMoreTextView extends androidx.appcompat.widget.AppCompatTextView
     public int mode;
     public int length;
     public static boolean is_open = false;
+    SpannableString ss;
+     TextView hee;
+     Context context;
     public SeeMoreTextView(Context context) {
         this(context, null);
     }
 
-    public SeeMoreTextView(final Context context, AttributeSet attrs) {
+    public SeeMoreTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        this.context = context;
         if (is_open == false){
             TypedArray property = context.obtainStyledAttributes(attrs, R.styleable.SeeMoreTextView);
             //
@@ -48,28 +56,70 @@ public class SeeMoreTextView extends androidx.appcompat.widget.AppCompatTextView
             //
             this.length = property.getInt(R.styleable.SeeMoreTextView_setLength,100);
             //
-            SpannableString ss;
+
             if (mode == 0){
-                //
-                final String[] lines = this.getText().toString().split("\\n");
-                if (lines.length == 1 || lines.length < length){
-                    //:D
-                }else {
-                    //
-                }
-                ss = new SpannableString(this.getText() + " " + TextOpenSeeMore);
+                hee = this;
+                this.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        int lineCount = hee.getLineCount();
+                        if (lineCount == 1 || lineCount < length){
+                            Handle(false);
+                        }else {
+                            Handle(true);
+                           // ss = new SpannableString(this.getText() + " " + TextOpenSeeMore);
+                           // this.setText(Event(ss));
+                          //  this.setMovementMethod(LinkMovementMethod.getInstance());
+                        }
+                    }
+                });
+
             }else{
-                ss = new SpannableString(this.getText() + " " + TextOpenSeeMore);
+                int lengthget = this.getText().toString().length();
+                if (lengthget <= length){
+                    ss = new SpannableString(this.getText());
+                    this.setText(ss);
+                }else{
+                    //ss = new SpannableString(this.getText().subSequence(start, end) + " " + TextOpenSeeMore);
+                    //this.setText(Event(ss));
+                    //this.setMovementMethod(LinkMovementMethod.getInstance());
+                }
             }
-            this.setText(Event(ss));
-            this.setMovementMethod(LinkMovementMethod.getInstance());
         }else {
 
         }
     }
+    String full = "";
+    public void Handle(Boolean state){
+        if (state == false){
+            ss = new SpannableString(this.getText());
+            this.setText(ss);
+        }else{
+            String last = "";
+            List<CharSequence> lines = new ArrayList<>();
+            //
+            full = this.getText().toString();
+            int count = hee.getLineCount();
+            int men = length -1;
+            for (int line = 0; line < length; line++) {
+                int start = hee.getLayout().getLineStart(line);
+                int end = hee.getLayout().getLineEnd(line);
+                CharSequence substring = hee.getText().subSequence(start, end);
+                last = last + substring;
+                if (line == men){
+                    last = last + " " + TextOpenSeeMore;
+                }
+            }
+
+            this.setText(Event(last));
+            this.setMovementMethod(LinkMovementMethod.getInstance());
+        }
+    }
+
 
     //This Functions Coming Soon :D
-    private SpannableString Event(SpannableString ss){
+    private SpannableString Event(String ss1){
+        SpannableString ss = SpannableString.valueOf(ss1);
         int start = ss.length() - TextOpenSeeMore.length();
         String hey = ss.subSequence(start,ss.length()).toString();
         if (hey.equals(TextOpenSeeMore)){
@@ -105,6 +155,6 @@ public class SeeMoreTextView extends androidx.appcompat.widget.AppCompatTextView
     }
 
     private void handle(){
-        //:)
+        this.setText(full + " " + "کوچک کردن");
     }
 }
